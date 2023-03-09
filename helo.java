df2bdfae0815bde33aee009f11ec0f907a271d60
Mohamad Aidil Najmir <aidilnaj@umich.edu>
2023-03-09 12:14:17 -0500

    edit loop to 10

--- helo.java
+++ helo.java
@@ -1,7 +1,7 @@
 public class helo {
     public static void main(String[] args) {
 
-        for(int i=1;i<=12;i++) {
+        for(int i=1;i<=10;i++) {
 
             System.out.println("Hello World!!!"+i);
         }
